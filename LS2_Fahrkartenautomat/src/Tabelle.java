d11cac6e4b56869d279ffe05980f5b98d0c133ca
Martin Frey <m.frey@pkn.de>
2022-11-09 13:33:43 +0100

    A 2.6/A3.1

---
+++ LS2_Fahrkartenautomat/src/Tabelle.java
@@ -0,0 +1,9 @@
+
+public class Tabelle {
+	public static void main(String[] args) {
+		String fahrenheit = "Fahrenheit";
+		String celsius = "Celsius";
+		
+		System.out.printf(" %-12s | %12s", fahrenheit, celsius);
+	}
+}
