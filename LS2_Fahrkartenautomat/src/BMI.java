69a943372efb55af956934e2d0b8187d029b7737
Martin Frey <m.frey@pkn.de>
2022-11-30 11:05:55 +0100

    A4.7.3 ist implementiert

--- LS2_Fahrkartenautomat/src/BMI.java
+++ LS2_Fahrkartenautomat/src/BMI.java
@@ -1,9 +1,40 @@
+import java.util.Scanner;
 
 public class BMI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner eingabe = new Scanner(System.in);
+		
+		System.out.print("Bitte geben Sie ihr Körpergewicht ein (in kg): ");
+		int gewicht = eingabe.nextInt();
+		
+		System.out.print("Bitte geben Sie Ihre Körpergröße ein (in cm): ");
+		double größe = eingabe.nextDouble();
+		
+		System.out.print("Bitte geben Sie Ihr Geschlecht ein (m/n): ");
+		char geschlecht = eingabe.next().charAt(0);
+		System.out.println();
+		
+		double bmi = gewicht / Math.pow((größe / 100), 2);
+		bmi = Math.round(bmi * 100) / 100.0;
+		
+		if(geschlecht == 'm') {
+			if(bmi < 20){
+				System.out.println("Sie haben Untergewicht. || BMI: " + bmi);
+			} else if(bmi > 20 && bmi <= 25) {
+				System.out.println("Sie haben Normalgewicht. || BMI: " + bmi);
+			} else {
+				System.out.println("Sie haben Übergewicht. || BMI: " + bmi);
+			}
+		}else {
+			if(bmi < 19){
+				System.out.println("Sie haben Untergewicht. || BMI: " + bmi);
+			} else if(bmi > 19 && bmi <= 24) {
+				System.out.println("Sie haben Normalgewicht. || BMI: " + bmi);
+			} else {
+				System.out.println("Sie haben Übergewicht. || BMI: " + bmi);
+			}
+		}
 	}
 
 }
